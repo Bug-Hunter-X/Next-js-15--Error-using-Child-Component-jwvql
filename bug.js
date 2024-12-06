64a8629bba2812af5bfff0b81cdd92a9eb054790
BugHunter-X <askzenai@gmail.com>
2024-12-06 06:48:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,19 @@
+```javascript
+// pages/index.js
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <p>This is a simple paragraph.</p>
+      <MyComponent/>
+    </div>
+  );
+}
+
+function MyComponent(){
+  return(
+    <p>This is a child component</p>
+  );
+}
+```
